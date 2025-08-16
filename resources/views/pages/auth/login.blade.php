8aab45f2f0237c9d4380616174a77150f1aa198c
stiven lovera <stivenlovera@gmail.com>
2025-08-15 22:32:56 -0400

    cambios para probar

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -53,7 +53,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
                                             Password<span class="text-error-500">*</span>
                                         </label>
                                         <div x-data="{ showPassword: false }" class="relative">
-                                            <input :type="showPassword ? 'text' : 'password'" type="email"
+                                            <input :type="showPassword ? 'text' : 'password'"
                                                 id="password" name="password" placeholder="Introduce tu contraseña"
                                                 class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
                                             <span @click="showPassword = !showPassword"
