d7dff571714c9e1c883419748a2d141270d77015
root <root@holding-lockers.com>
2025-10-28 01:32:00 +0100

    cambios desde server

--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -1,15 +1,35 @@
 <?php
+
 namespace App\Services;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use stdClass;
 
-class LoginService
+class LoginService 
 {
-    public function __construct()
-    {}
+    public function __construct() {}
 
-    public function getCompany($users_id)
+    public function getCLientDefault($users_id)
+    {
+        Log::info("LoginService getCLientDefault " . jsonLog($users_id));
+        $empresa = DB::table('user_client')
+            ->where('user_client.user_id', $users_id)
+            ->first();
+        return  $empresa;
+    }
+
+    public function getChangeCompany($users_id, $client_id)
+    {
+        Log::info("LoginService getChangeCompany " . jsonLog($users_id));
+        $empresa = DB::table('user_client')
+            ->where('user_client.user_id', $users_id)
+            ->where('user_client.client_id', $client_id)
+            ->first();
+        return  $empresa;
+    }
+
+     public function getCompany($users_id)
     {
         Log::info("LoginService getCompany " . jsonLog($users_id));
         $company = DB::table('user_company')
@@ -33,15 +53,5 @@ class LoginService
             ->where('users_rol.user_id', $users_id)
             ->get();
         return $rol_building;
-    }
-
-    public function getChangeCompany($users_id, $client_id)
-    {
-        Log::info("LoginService getChangeCompany " . jsonLog($users_id));
-        $empresa = DB::table('user_client')
-            ->where('user_client.user_id', $users_id)
-            ->where('user_client.client_id', $client_id)
-            ->first();
-        return $empresa;
     }
 }
