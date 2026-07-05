93879821e5d18d762ba9a47b3a6ae746a394b109
stiven lovera <stivenlovera@gmail.com>
2026-07-04 20:59:40 -0400

    cambio en la master

--- app/Services/AppMovil/DatabaseService.php
+++ app/Services/AppMovil/DatabaseService.php
@@ -16,41 +16,50 @@ class DatabaseService
         Log::info("DatabaseService GetDataBase locker_id " . jsonLog(getLocker()));
 
         $department = DB::table('department')
+        ->select('department.*')
             ->where('department.building_id', getLocker()->get('building_id'))
             ->get();
 
         $lockers = DB::table('locker')
+        ->select('locker.*')
             ->where('locker.locker_id', getLocker()->get('locker_id'))
             ->get();
 
         $users = DB::table('user')
+            ->select('user.*')
             ->join('department', 'user.department_id', '=', 'department.department_id')
             ->where('department.building_id', getLocker()->get('building_id'))
             ->get();
 
         $controllers = DB::table('controller')
+            ->select('controller.*')
             ->where('controller.locker_id', getLocker()->get('locker_id'))
             ->get();
 
         $door_sizes = DB::table('door_size')
+            ->select('door_size.*')
             ->get();
 
         $doors = DB::table('door')
+            ->select('door.*')
             ->join('controller', 'controller.controller_id', '=', 'door.controller_id')
             ->where('controller.locker_id', getLocker()->get('locker_id'))
             ->get();
 
         $movements = DB::table('movement')
+            ->select('movement.*')
             ->where('movement.building_id', getLocker()->get('building_id'))
             ->get();
 
         $request_comand = DB::table('request_comand')
+            ->select('request_comand.*')
             ->join('door', 'door.door_id', '=', 'request_comand.door_id')
             ->join('controller', 'controller.controller_id', '=', 'door.controller_id')
             ->where('controller.locker_id', getLocker()->get('locker_id'))
             ->get();
 
         $response_comand = DB::table('response_comand')
+            ->select('response_comand.*')
             ->join('request_comand', 'request_comand.request_comand_id', '=', 'response_comand.request_comand_id')
             ->join('door', 'door.door_id', '=', 'request_comand.door_id')
             ->join('controller', 'controller.controller_id', '=', 'door.controller_id')
